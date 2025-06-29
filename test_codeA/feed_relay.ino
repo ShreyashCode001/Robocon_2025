1fe4be404eb06087913d3be7a6d6e7fd5d536dff
Shreyash <shreyashgourkar001@gmail.com>
2025-06-29 23:45:06 +0530

    Delete test_codeA/feed_relay.ino

--- test_codeA/feed_relay.ino
+++
@@ -1,9 +0,0 @@
-void feedrelay() {
-  if (ps5.Down()) {
-    digitalWrite(feed_relay, HIGH);  // high input >> Active low input >> relay off >> for piston ON >> Normally closed circuit
-    Serial.println("Feed piston ON");
-  } else {
-    digitalWrite(feed_relay, LOW); // Low input >> Active low input >> relay ON >> for piston OFF >> Normally closed circuit
-    Serial.println("Feed piston OFF");
-  }
-}
